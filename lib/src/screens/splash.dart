d8c82fa56b289aa9a39fe5f2458f6280932311a1
Dipanshu Singh <dipanshu.singh1997@gmail.com>
2021-03-23 18:39:13 +0530

    removes route animation and replaces page route builder with material page route

--- lib/src/screens/splash.dart
+++ lib/src/screens/splash.dart
@@ -14,12 +14,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
     /// this will navigate to [HomeScreen] after 3 sec.
     Future.delayed(Duration(seconds: 3), () {
-      Navigator.of(context).pushReplacement(
-        PageRouteBuilder(
-          pageBuilder: (context, animation, secondaryAnimation) => HomeScreen(),
-          transitionsBuilder: _getSlideAnimation,
-        ),
-      );
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => HomeScreen()));
     });
   }
 
@@ -30,19 +25,6 @@ class _SplashScreenState extends State<SplashScreen> {
       body: Center(
         child: Image.asset('assets/gifs/hn.gif'),
       ),
-    );
-  }
-
-  Widget _getSlideAnimation(BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
-    var begin = Offset(0.0, 1.0);
-    var end = Offset.zero;
-    var curve = Curves.ease;
-
-    var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-
-    return SlideTransition(
-      position: animation.drive(tween),
-      child: child,
     );
   }
 }

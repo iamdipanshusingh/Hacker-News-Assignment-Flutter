b5388dab4e23e1b9e69abbca2cd4f0895b9db62f
Dipanshu Singh <dipanshu.singh1997@gmail.com>
2021-03-23 19:49:04 +0530

    adds API urls

--- lib/src/utils/const.dart
+++ lib/src/utils/const.dart
@@ -1,3 +1,8 @@
 import 'package:flutter/material.dart';
 
+/// colors
 const Color bgColor = Color(0xfff6f6f6);
+
+/// urls
+const BASE_URL = 'http://hn.algolia.com/api/v1/';
+const SEARCH_URL = BASE_URL + 'search?query=';

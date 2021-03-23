375602e7af9b6d6a7cd95cc942806d6ce750e2fe
Dipanshu Singh <dipanshu.singh1997@gmail.com>
2021-03-23 19:50:09 +0530

    adds comments in provider method

--- lib/src/provider/state.dart
+++ lib/src/provider/state.dart
@@ -8,6 +8,9 @@ class AppState extends ChangeNotifier {
   bool get isLoading => _isLoading;
   List<NewsResult> get newsList => _newsList;
 
+  /// this will simply search the news
+  ///
+  /// will be used to update the loading behaviour, fetching the news list
   searchNews(String query, {bool shouldNotify = true}) {
     print('$query has been searched');
   }

fe587d7e543ba03d28aa4fdbb4d734d61d01ea5b
Dipanshu Singh <dipanshu.singh1997@gmail.com>
2021-03-23 18:33:09 +0530

    adds basic home screen

--- lib/src/screens/splash.dart
+++ lib/src/screens/splash.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:searchhn/src/screens/home/home.dart';
 import 'package:searchhn/src/utils/const.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -8,12 +9,40 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+
+    /// this will navigate to [HomeScreen] after 3 sec.
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+        PageRouteBuilder(
+          pageBuilder: (context, animation, secondaryAnimation) => HomeScreen(),
+          transitionsBuilder: _getSlideAnimation,
+        ),
+      );
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: bgColor,
       body: Center(
         child: Image.asset('assets/gifs/hn.gif'),
       ),
+    );
+  }
+
+  Widget _getSlideAnimation(BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
+    var begin = Offset(0.0, 1.0);
+    var end = Offset.zero;
+    var curve = Curves.ease;
+
+    var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+    return SlideTransition(
+      position: animation.drive(tween),
+      child: child,
     );
   }
 }

c7de95634c0fb5de888083ab028c1a25f61bd26e
Dipanshu Singh <dipanshu.singh1997@gmail.com>
2021-03-23 17:59:47 +0530

    ui: adds splash screen (#1)
    * adds custom gif for splash
    
    * creates splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:searchhn/src/screens/splash.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,6 +8,10 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      title: 'Search Hacker News',
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
   }
 }

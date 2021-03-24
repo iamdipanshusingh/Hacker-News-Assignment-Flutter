4aa6d8825d91f116cf044368bfbd2588ac4abfdd
Dipanshu Singh <dipanshu.singh1997@gmail.com>
2021-03-24 10:52:40 +0530

    adds a bare minimum news details screen

---
+++ lib/src/screens/news_details/news_details.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class NewsDetailsScreen extends StatefulWidget {
+  final String id;
+
+  const NewsDetailsScreen({Key key, this.id}) : super(key: key);
+
+  @override
+  _NewsDetailsScreenState createState() => _NewsDetailsScreenState();
+}
+
+class _NewsDetailsScreenState extends State<NewsDetailsScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

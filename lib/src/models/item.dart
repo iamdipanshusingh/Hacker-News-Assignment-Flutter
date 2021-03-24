0d2d7609724f70b1c16d0e4a6a9e2c823c109924
Dipanshu Singh <dipanshu.singh1997@gmail.com>
2021-03-24 11:00:31 +0530

    adds item details model

---
+++ lib/src/models/item.dart
@@ -0,0 +1,34 @@
+class ItemDetails {
+  final int id;
+  final String type;
+  final String author;
+  final String title;
+  final String url;
+  final String text;
+  final List<ItemDetails> children;
+
+  ItemDetails({
+    this.id,
+    this.type,
+    this.author,
+    this.title,
+    this.url,
+    this.text,
+    this.children,
+  });
+
+  factory ItemDetails.fromJson(var json) {
+    List children = json['children'];
+    List<ItemDetails> parsedChildren = children.map((item) => ItemDetails.fromJson(item)).toList();
+
+    return ItemDetails(
+      id: json['id'],
+      type: json['type'],
+      author: json['author'],
+      title: json['title'],
+      url: json['url'],
+      text: json['text'],
+      children: parsedChildren,
+    );
+  }
+}
